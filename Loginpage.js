ce3eced6c64f6cf5e45bc8afa90bc1748e374ced
kaurjaismeen29 <102655937+kaurjaismeen29@users.noreply.github.com>
2022-05-15 00:16:32 +0530

    updated

--- Loginpage.js
+++ Loginpage.js
@@ -1,1 +1,13 @@
+function login(){
+    var uname = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
 
+    if (uname=="admin" & pass=="123"){
+        location.assign("file:///E:/Project/Classes.html");
+        window.alert("Login Successful")
+    }
+    else
+    {
+        window.alert("Login Failed")
+    }
+}
